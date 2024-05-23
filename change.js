974e9cc53e306195d739ac6bfd79fc30cc68519b
Bielango <95250987+Bielango@users.noreply.github.com>
2024-05-23 12:56:52 -0300

    Add files via upload

---
+++ change.js
@@ -0,0 +1,28 @@
+function changeHeadingText() {
+  var titulo_sec = document.getElementById("titulo_sec");
+  titulo_sec.innerHTML = "High Tech";
+}
+
+function changeHeadingText2() {
+  var titulo_sec = document.getElementById("titulo_sec");
+  titulo_sec.innerHTML = "Emporio";
+}
+
+function alerta() {
+  alert("ola, isto é um alerta");
+}
+
+// não utilizado
+// function Pessoa(nome, idade) {
+//   this.nome = nome;
+//   this.idade = idade || 1;
+// }
+
+// function criarPessoa() {
+//   var nomeP = document.getElementById("nomeP");
+//   var idadeP = document.getElementById("idadeP");
+//   pessoa1.Pessoa(nomeP, idadeP);
+// }
+// function alerta2() {
+//   alert("pessoa1");
+// }
